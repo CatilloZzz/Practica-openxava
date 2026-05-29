e459ce20c9fce6f63eac7e899213b5743410a4da
CatilloZzz <jireyes@uamv.edu.ni>
2026-05-29 15:41:17 -0600

    Se agrego la clase Cliente

--- src/main/java/ni/edu/uam/Facturacion/model/Cliente.java
+++ src/main/java/ni/edu/uam/Facturacion/model/Cliente.java
@@ -1,4 +1,19 @@
 package ni.edu.uam.Facturacion.model;
 
+import lombok.*;
+import org.openxava.annotations.*;
+import javax.persistence.*;
+@Entity
+@Getter@Setter
 public class Cliente {
+    @Id
+    @Column(length = 6)
+    int numero;
+
+
+    @Id
+    @Column(length = 50)
+    String nombre;
+
+
 }
